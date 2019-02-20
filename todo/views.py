0a45588b2f5c89db322b31ac87de4c948678c7e1
Prince Oppong Boamah <peedor@Princes-MacBook-Pro.local>
2019-02-20 04:51:36 +0000

    Prince: Created a view for the app

--- todo/views.py
+++ todo/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .models import TodoItem
+from todo.forms.todo_form import TodoForm
 
-def todoView(request):
-	all_todo_items = TodoItem.objects.all()
-	return render(request, 'index.html', {'all_items': all_todo_items})
+def index(request):
+	item_form = TodoForm()
+	items = TodoItem.objects.all()
+	return render(request, 'todo/index.html', {'form': item_form, 'items':items})
 
-def addTodo(request):
-	new_item = TodoItem(content = request.POST['content'])
-	new_item.save()
-	return HttpResponseRedirect('/todo/')
+def save(request):
+    item_form = TodoForm(request.POST)
+
+    if item_form.is_valid():
+        new_item = TodoItem()
+        new_item.content = item_form.cleaned_data['content']
+        new_item.author = item_form.cleaned_data['author']
+        new_item.date_created = item_form.cleaned_data['date_created']
+        new_item.completed = item_form.cleaned_data['completed']
+        new_item.save()
+        return HttpResponseRedirect('/todo')
+
+    else:
+        return render(request, 'todo/index.html', {'form': item_form})
