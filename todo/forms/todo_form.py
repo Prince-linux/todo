cecfc325ffcfe04ad28232f3b41d6b1174c1b075
Prince Oppong Boamah <peedor@Princes-MacBook-Pro.local>
2019-02-25 20:00:25 +0000

    Prince: No update

--- todo/forms/todo_form.py
+++ todo/forms/todo_form.py
@@ -8,23 +8,6 @@ class TodoForm(forms.Form):
     date_created = forms.DateTimeField()
     completed = forms.BooleanField(required=False,  initial=False, label='Completed')
 
-#    def clean_author(self):
-#        cleaned_data = super(TodoForm, self).clean()
-#        content = cleaned_data.get('content')
-#        author = cleaned_data.get('author', 'Mr.', 'Mrs.', 'Dr.')
-#        date_created = cleaned_data.get('date_completed')
-#        completed = cleaned_data.get('completed')
-#
-#        if not content and not author and not date_created and not completed:
-#            raise forms.ValidationError('You have to fill in with the right inputs')
-
-
-#    def clean_author(self):
-#        if not (self.cleaned_data.get('author')
-#                    .startswith('Mr.', 'Mrs.', 'Dr.')):
-#            raise forms.ValidationError("Please Start with either Mr. , Mrs. or Dr. ")
-#
-#        return self.cleaned_data.get('author')
 
 
 
