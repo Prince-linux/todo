7a3a23a98b160d98a7052f130ba82d0b5d43105d
Prince Oppong Boamah <peedor@Princes-MacBook-Pro.local>
2019-02-25 19:59:00 +0000

    Prince: Added urls to create, delete, and change values

--- to_do/urls.py
+++ to_do/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from todo.views import index, save, lists
+from todo.views import index, save, lists, mark_as_done, mark_as_undone, delete, delete_all, create_todo, create_new
 
 urlpatterns = [
     # Examples:
@@ -11,5 +11,11 @@ urlpatterns = [
     url(r'^todo/$',index, name="main-view"),
     url(r'^new/$', save, name="addTodo"),
     url(r'^list/$', lists, name = "list_todo"),
+    url(r'^mark/(?P<item_id>\d+)/done/$', mark_as_done),
+    url(r'^mark/(?P<item_id>\d+)/undone/$', mark_as_undone),
+    url(r'^delete(?P<item_id>\d+)/$', delete),
+    url(r'^delete_all/$', delete_all),
+    url(r'^create/$', create_todo),
+    url(r'^create_new/$', create_new)
 
 ]
